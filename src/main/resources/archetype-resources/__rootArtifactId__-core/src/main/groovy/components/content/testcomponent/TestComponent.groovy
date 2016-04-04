da028d07da0a3e96361fb00fad3309f3c39225bf
Mark Daugherty <mark.r.daugherty@gmail.com>
2016-04-04 16:17:59 -0500

    update.

--- src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/groovy/components/content/testcomponent/TestComponent.groovy
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/groovy/components/content/testcomponent/TestComponent.groovy
@@ -3,7 +3,7 @@ package ${package}.components.content.testcomponent;
 import com.citytechinc.cq.component.annotations.Component
 import com.citytechinc.cq.component.annotations.DialogField
 import com.citytechinc.cq.component.annotations.widgets.TextField
-import com.citytechinc.aem.bedrock.core.components.AbstractComponent
+import com.icfolson.aem.library.core.components.AbstractComponent
 import com.icfolson.aem.library.api.components.annotations.AutoInstantiate
 import org.apache.sling.api.resource.Resource
 import org.apache.sling.models.annotations.Model
@@ -12,10 +12,10 @@ import ${package}.annotations.AEMComponent
 import javax.inject.Inject
 
 @AEMComponent("Test Component")
-public class TestComponent extends AbstractComponent {
+class TestComponent extends AbstractComponent {
 
-	@DialogField(fieldLabel = "Test Title", fieldDescription = "Our title")
+	@DialogField(fieldLabel = "Test Title", fieldDescription = "This is the test title.")
 	@TextField
     @Inject
-    private String title;
+    String title
 }
