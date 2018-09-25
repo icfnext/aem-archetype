b28f7b382d30dbde4ac6b76c51fa43bf01e42008
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-09-25 16:45:56 -0500

    added archetype resources for editable templates.

--- README.md
+++ README.md
@@ -4,17 +4,16 @@
 
 ## Compatibility
 
-For use with AEM 6.4
+For use with AEM 6.4.
 
 ## Dependency Versions
 
 | Dependency  | Version |
 | ----------- | ------- |
-| AEM Library | 11.0.0 |
-| Multicomposite Add On | 3.2.2 |
+| AEM Library | 11.1.0 |
+| Multicomposite Add On | 4.0.0 |
 | CQ Component Maven Plugin | 5.1.0 |
 | OSGi Bundle Status Maven Plugin | 2.0.2 |
-| AEM Package Maven Plugin | 0.7.0 |
 
 ## Example Usage
 
--- src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/groovy/annotations/AEMComponent.groovy
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/groovy/annotations/AEMComponent.groovy
@@ -1,12 +1,14 @@
 package ${package}.annotations
 
-import groovy.transform.AnnotationCollector
 import com.citytechinc.cq.component.annotations.Component
+import groovy.transform.AnnotationCollector
 import org.apache.sling.api.resource.Resource
+import org.apache.sling.models.annotations.DefaultInjectionStrategy
 import org.apache.sling.models.annotations.Model
 
 @AnnotationCollector
-@Component(group = "")
-@Model(adaptables = Resource)
+@Component(value = "Component")
+@Model(adaptables = Resource, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @interface AEMComponent{
+
 }
